193dfe2d3273c2056bdec5d7ca15a66ed5c7306d
yestraa99 <https://github.com/yestraa99>
2022-10-26 17:22:41 +0900

    숙제시작

---
+++ src/main/java/kh/study/gitTest/GitHomeWork3Application.java
@@ -0,0 +1,14 @@
+package kh.study.gitTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHomeWork3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHomeWork3Application.class, args);
+		System.out.println("강의실에서 보냄 숙제");
+	}
+
+}
