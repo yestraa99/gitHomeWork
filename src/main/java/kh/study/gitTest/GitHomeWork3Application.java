c82e04d96a34afc8091114404a6232de3fea9589
yestraa99 <https://github.com/yestraa99>
2022-10-26 17:28:34 +0900

    파일어디로

--- src/main/java/kh/study/gitTest/GitHomeWork3Application.java
+++ src/main/java/kh/study/gitTest/GitHomeWork3Application.java
@@ -9,6 +9,7 @@ public class GitHomeWork3Application {
 	public static void main(String[] args) {
 		SpringApplication.run(GitHomeWork3Application.class, args);
 		System.out.println("강의실에서 보냄 숙제");
+		System.out.println("파일테스트");
 	}
 
 }
